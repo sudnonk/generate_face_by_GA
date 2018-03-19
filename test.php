63c4813d65767627e099f6ac3bd182d744f59aac
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-20 00:17:43 +0900

    間違えて猫の顔使ってた

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
-    $fDtc = face_detect("test_face.png","haarcascade_frontalcatface.xml");
+
+    $fDtc = face_detect("test_face.png","haarcascade_frontalface_default.xml");
     var_dump($fDtc);
 
-    $fDtc = face_detect("hyoujou_shinda_me_man.png","haarcascade_frontalcatface.xml");
+    $fDtc = face_detect("hyoujou_shinda_me_man.png","haarcascade_frontalface_default.xml");
     var_dump($fDtc);
