a17b02d3902b5b7d5faa4f7f409f85ed1cbd643b
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-20 00:50:23 +0900

    OpenCVはもう使わない

--- test.php
+++ test.php
@@ -1,7 +1,1 @@
 <?php
-
-    $fDtc = face_detect("test_face.png","haarcascade_frontalface_default.xml");
-    var_dump($fDtc);
-
-    $fDtc = face_detect("hyoujou_shinda_me_man.png","haarcascade_frontalface_default.xml");
-    var_dump($fDtc);
