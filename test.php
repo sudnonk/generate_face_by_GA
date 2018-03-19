e01549638a61443fc5ca3f5a27a5ae30eb3714ba
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-20 01:34:35 +0900

    create goal.png

--- test.php
+++ test.php
@@ -1,11 +1,23 @@
 <?php
     $im = imagecreatefrompng("goal.png");
 
-    $im2 = imagecreate(256, 256);
+    $im2   = imagecreate(256, 256);
+    $black = imagecolorallocate($im2, 0, 0, 0);
+    $white = imagecolorallocate($im2, 255, 255, 255);
 
     for ($i = 0; $i < 256; $i++) {
         for ($j = 0; $j < 256; $j++) {
-            $rgb = imagecolorat($im,$i,$j);
-            var_dump($rgb);
+            $rgb = imagecolorat($im, $i, $j);
+            $r   = ($rgb >> 16) & 0xFF;
+            $g   = ($rgb >> 8) & 0xFF;
+            $b   = $rgb & 0xFF;
+
+            if ($r > 127 && $g > 127 && $b > 127) {
+                imagesetpixel($im2, $i, $j, $black);
+            } else {
+                imagesetpixel($im2, $i, $j, $white);
+            }
         }
     }
+
+    imagepng($im2, "goal2.png", 0);
