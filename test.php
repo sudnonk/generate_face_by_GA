82ccd27635d92ed468a771037cff536103ae1592
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-20 01:25:41 +0900

    create goal.png

--- test.php
+++ test.php
@@ -1,1 +1,11 @@
 <?php
+    $im = imagecreatefrompng("goal.png");
+
+    $im2 = imagecreate(256, 256);
+
+    for ($i = 0; $i < 256; $i++) {
+        for ($j = 0; $j < 256; $j++) {
+            $rgb = imagecolorat($im,$i,$j);
+            var_dump($rgb);
+        }
+    }
