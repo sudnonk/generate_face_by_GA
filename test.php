84ca78a29c4274d447f6326882d1322bea0dd312
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-20 01:49:32 +0900

    create goal.png

--- test.php
+++ test.php
@@ -8,19 +8,14 @@
     for ($i = 0; $i < 256; $i++) {
         for ($j = 0; $j < 256; $j++) {
             $rgb = imagecolorat($im, $i, $j);
-            $r   = ($rgb >> 16) & 0xFF;
-            $g   = ($rgb >> 8) & 0xFF;
-            $b   = $rgb & 0xFF;
-header("Content-Type: text/plain");
-            var_dump($r,$g,$b);
 
-            if ($r > 127 && $g > 127 && $b > 127) {
+            if ($rgb > 127) {
                 imagesetpixel($im2, $i, $j, $black);
             } else {
                 imagesetpixel($im2, $i, $j, $white);
             }
         }
     }
-exit();
+
     header("Content-Type: image/png");
     imagepng($im2);
