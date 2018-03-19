53b359aeb3f1a00021fb55d95ab9a0ec2c084b4d
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-20 00:08:09 +0900

    add test file2

--- test.php
+++ test.php
@@ -1,3 +1,6 @@
 <?php
     $fDtc = face_detect("test_face.png","/usr/local/src/opencv/data/haarcascades/haarcascade_frontalcatface.xml");
     var_dump($fDtc);
+
+    $fDtc = face_detect("hyoujou_shinda_me_man.png","/usr/local/src/opencv/data/haarcascades/haarcascade_frontalcatface.xml");
+    var_dump($fDtc);
