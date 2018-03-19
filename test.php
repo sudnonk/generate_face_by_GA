8ad80ed294abeb4cb5b397526a18ada363a425fe
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-20 00:11:40 +0900

    add cascade file from opencv

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
-    $fDtc = face_detect("test_face.png","/usr/local/src/opencv/data/haarcascades/haarcascade_frontalcatface.xml");
+    $fDtc = face_detect("test_face.png","haarcascade_frontalcatface.xml");
     var_dump($fDtc);
 
-    $fDtc = face_detect("hyoujou_shinda_me_man.png","/usr/local/src/opencv/data/haarcascades/haarcascade_frontalcatface.xml");
+    $fDtc = face_detect("hyoujou_shinda_me_man.png","haarcascade_frontalcatface.xml");
     var_dump($fDtc);
