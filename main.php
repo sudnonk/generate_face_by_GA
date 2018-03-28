8539c5b678a0cc54af1a4c3f2b122c978d2846f6
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-28 17:55:13 +0900

    fix

--- main.php
+++ main.php
@@ -5,8 +5,8 @@
     $genes = array();
     for ($x = 0; $x < Individual::width; $x++) {
         for ($y = 0; $y < Individual::height; $y++) {
-            /** @var Color $color */
-            $color   = new Color(mt_rand(0, 7));
+            /** @var int $color */
+            $color   = mt_rand(0, 7);
             $genes[] = new Gene($x, $y, $color);
         }
     }
