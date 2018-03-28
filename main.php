73941a3f0e1c37adf93dbe19f59ae67cb9ca455e
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-28 17:56:55 +0900

    1 individualで9MBぐらい

--- main.php
+++ main.php
@@ -1,7 +1,6 @@
 <?php
     require "init.php";
 
-    var_dump(memory_get_usage());
     $genes = array();
     for ($x = 0; $x < Individual::width; $x++) {
         for ($y = 0; $y < Individual::height; $y++) {
@@ -10,9 +9,8 @@
             $genes[] = new Gene($x, $y, $color);
         }
     }
-    var_dump(memory_get_usage());
+
     $individual = new Individual($genes);
-    var_dump(memory_get_usage());
-    exit();
+
     header("Content-Type:image/png");
     imagepng($individual->to_image());
