c080aa9090f194c61277f08069a79edf4794b693
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-30 00:08:19 +0900

    debug

--- imgs/gallery.php
+++ imgs/gallery.php
@@ -5,7 +5,7 @@
     $dirs = scandir($path);
     foreach ($dirs as $dir) {
         if ($dir === "." || $dir === "..") continue;
-
+var_dump($dir);
         echo "<h2>" . date("Y年m月d日 H時i分s秒", strtotime($dir)) . "</h2>";
         foreach (glob("$dir/*.png") as $file) {
             echo "<img src='" . $path . $dir . DIRECTORY_SEPARATOR . $file . "'>";
