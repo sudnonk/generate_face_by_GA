efa3e1f25dd997306dac2d0478a842ba8513e25a
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-30 00:07:18 +0900

    画像一覧を見れるページ

---
+++ imgs/gallery.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php
+    $path = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+    $dirs = scandir($path);
+    foreach ($dirs as $dir) {
+        if ($dir === "." || $dir === "..") continue;
+
+        echo "<h2>" . date("Y年m月d日 H時i分s秒", strtotime($dir)) . "</h2>";
+        foreach (glob("$dir/*.png") as $file) {
+            echo "<img src='" . $path . $dir . DIRECTORY_SEPARATOR . $file . "'>";
+        }
+    }
+?>
+</body>
+</html>
+
+
