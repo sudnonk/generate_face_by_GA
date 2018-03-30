ad0cb1f4cc48e62d7fdf057f8f8ee17bbc0c938a
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-30 10:21:08 +0900

    Revert "みす"
    This reverts commit 250b1f6

--- Gene.php
+++ Gene.php
@@ -2,11 +2,45 @@
 
 
     final class Gene {
+        /** @var int $x */
+        private $x;
+        /** @var int $y */
+        private $y;
         /** @var int $color_code */
         private $color_code;
 
-        public function __construct(int $color) {
+        public function __construct(int $x, int $y, int $color) {
+            $this->setX($x);
+            $this->setY($y);
             $this->setColorCode($color);
+        }
+
+        /**
+         * @return int
+         */
+        final public function getX(): int {
+            return $this->x;
+        }
+
+        /**
+         * @param int $x
+         */
+        final private function setX(int $x): void {
+            $this->x = $x;
+        }
+
+        /**
+         * @return int
+         */
+        final public function getY(): int {
+            return $this->y;
+        }
+
+        /**
+         * @param int $y
+         */
+        final private function setY(int $y): void {
+            $this->y = $y;
         }
 
         /**
@@ -30,6 +64,9 @@
          * @return Gene
          */
         final public static function cross(Gene $parent1, Gene $parent2): Gene {
-            return new Gene(Color::mix($parent1->getColorCode(), $parent2->getColorCode()));
+            if ($parent1->getX() !== $parent2->getX() || $parent1->getY() !== $parent2->getY()) {
+                die("cross invalid.");
+            }
+            return new Gene($parent1->getX(), $parent1->getY(), Color::mix($parent1->getColorCode(), $parent2->getColorCode()));
         }
     }
