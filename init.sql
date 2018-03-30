4501f082ce2738a24a135407944195c74096f3d6
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-30 10:13:54 +0900

    primary key

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE individual (
-  id         INT  NOT NULL AUTO_INCREMENT,
+  id         INT  NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   generation INT  NOT NULL,
   distance   INT  NOT NULL,
   is_elite   BOOL NOT NULL,
