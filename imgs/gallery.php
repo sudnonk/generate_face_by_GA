1649a4cb46d563789aaf0de77b3e0113bc9a2f90
Yuki <shipyuuki@yahoo.co.jp>
2018-04-03 23:00:14 +1000

    base64で画像表示

--- imgs/gallery.php
+++ imgs/gallery.php
@@ -1,17 +1,18 @@
 <html>
 <body>
 <?php
-    $path = dirname(__DIR__) . DIRECTORY_SEPARATOR . "imgs" . DIRECTORY_SEPARATOR;
-    $dirs = scandir($path);
-    foreach ($dirs as $dir) {
-        if ($dir === "." || $dir === "..") continue;
-        if (!is_dir($dir)) continue;
+$path = dirname(__DIR__) . DIRECTORY_SEPARATOR . "imgs" . DIRECTORY_SEPARATOR;
+$dirs = scandir($path);
+foreach ($dirs as $dir) {
+    if ($dir === "." || $dir === "..") continue;
+    if (!is_dir($dir)) continue;
 
-        echo "<h2>" . date("Y年m月d日 H時i分s秒", strtotime($dir)) . "</h2>";
-        foreach (glob("$dir/*.png") as $file) {
-            echo "<img src='" . $path . $file . "'>";
-        }
+    echo "<h2>" . date("Y年m月d日 H時i分s秒", strtotime($dir)) . "</h2>";
+    foreach (glob("$dir/*.png") as $file) {
+        $base64 = base64_encode(file_get_contents($path . $file));
+        echo "<img src='data:image/png;base64," . $base64 . "'>";
     }
+}
 ?>
 </body>
 </html>
