8d36897d90613f7569e0e3c2946bc5085c8394fb
しとざわ <sudnonk12@yahoo.co.jp>
2018-04-04 17:51:52 +0900

    過去の実験を見るページ

--- log/index.php
+++ log/index.php
@@ -2,14 +2,14 @@
     require "../init.php";
 
     $expnums = Database::getPastExpNums();
-    ?>
+?>
 <html>
 <body>
 <form method="get" action="show.php">
     <select name="expnum">
-        <?foreach ($expnums as $expnum):?>
-        <option value="<?=$expnum?>"><?=$expnum?></option>
-        <?php endforeach;?>
+        <?php foreach ($expnums as $expnum): ?>
+            <option value="<?= $expnum ?>"><?= $expnum ?></option>
+        <?php endforeach; ?>
     </select>
     <input type="submit">
 </form>
